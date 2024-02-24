e7869e9fdf3d0dab7d284244984be3b89f0c59d2
BrainlessNinja <kanagsn3@gmail.com>
2024-02-24 17:43:18 +0600

    Math tasks

---
+++ math/task1.py
@@ -0,0 +1,8 @@
+import math
+
+d = int(input("Input degree: "))
+
+radian = (d*math.pi)/180
+
+
+print(radian)
---
+++ math/task2.py
@@ -0,0 +1,7 @@
+import math
+
+height = int(input("Input height: "))
+base1 = int(input("Input first base: "))
+base2 = int(input("Input second value: "))
+
+print(((base1+base2)/2)*height)
